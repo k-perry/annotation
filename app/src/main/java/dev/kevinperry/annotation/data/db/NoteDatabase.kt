4026be56e757087979958b98ccb1c1fabda1a28e
Kevin Perry <kevin.perry@ncr.com>
2021-05-04 16:11:21 -0400

    Optimize imports in NoteDatabase

--- app/src/main/java/dev/kevinperry/annotation/data/db/NoteDatabase.kt
+++ app/src/main/java/dev/kevinperry/annotation/data/db/NoteDatabase.kt
@@ -2,13 +2,7 @@ package dev.kevinperry.annotation.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import dev.kevinperry.annotation.data.model.Note
-import dev.kevinperry.annotation.di.ApplicationScope
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import javax.inject.Inject
-import javax.inject.Provider
 
 @Database(entities = [Note::class], version = 1)
 abstract class NoteDatabase : RoomDatabase() {
